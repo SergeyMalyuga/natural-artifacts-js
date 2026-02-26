9066cb2417719f9b4700ca4a11c81fe5cbe23c51
Sergey Malyuga <sergey@malyuga.ru>
2026-02-26 09:38:46 +0300

    feat(artefact-state.ts): создать тип данных

--- src/store/slices/artefactSlice.ts
+++ src/store/slices/artefactSlice.ts
@@ -1,56 +1,64 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 import {Artefact} from "../../types/artefact.ts";
+import {ArtefactsState} from "../../types/artefacts-state.ts";
 
-const initialState: Artefact[] = [
-    {
-        image: {
-            url: "./images/raster/artefacts-plant.png",
-            height: 223,
-            width: 120,
+const initialState: ArtefactsState = {
+    artefacts: [
+        {
+            image: {
+                url: "./images/raster/artefacts-plant.png",
+                height: 223,
+                width: 120,
+            },
+            title: "Fig. 1 (plant)",
+            description:
+                "Имеется спорная точка зрения, гласящая примерно следующее: активно развивающиеся страны третьего мира " +
+                "своевременно верифицированы.",
         },
-        title: "Fig. 1 (plant)",
-        description:
-            "Имеется спорная точка зрения, гласящая примерно следующее: активно развивающиеся страны третьего мира " +
-            "своевременно верифицированы.",
-    },
-    {
-        image: {
-            url: "./images/raster/artefacts-flower.png",
-            height: 197,
-            width: 120,
+        {
+            image: {
+                url: "./images/raster/artefacts-flower.png",
+                height: 197,
+                width: 120,
+            },
+            title: "Fig. 2 (flower)",
+            description:
+                "Прежде всего, синтетическое тестирование влечет за собой процесс внедрения и модернизации условий.",
         },
-        title: "Fig. 2 (flower)",
-        description:
-            "Прежде всего, синтетическое тестирование влечет за собой процесс внедрения и модернизации условий.",
-    },
-    {
-        image: {
-            url: "./images/raster/artefacts-leaf.png",
-            height: 204,
-            width: 120,
+        {
+            image: {
+                url: "./images/raster/artefacts-leaf.png",
+                height: 204,
+                width: 120,
+            },
+            title: "Fig. 3 (leaf)",
+            description:
+                "Лишь непосредственные участники прогресса неоднозначны и будут в равной степени предоставлены сами себе для работы.",
         },
-        title: "Fig. 3 (leaf)",
-        description:
-            "Лишь непосредственные участники прогресса неоднозначны и будут в равной степени предоставлены сами себе для работы.",
-    },
-    {
-        image: {
-            url: "./images/raster/artefacts-wood.png",
-            height: 226,
-            width: 120,
+        {
+            image: {
+                url: "./images/raster/artefacts-wood.png",
+                height: 226,
+                width: 120,
+            },
+            title: "Fig. 4 (wood)",
+            description:
+                "Базовый вектор развития не даёт нам иного выбора, кроме определения новых предложений.",
         },
-        title: "Fig. 4 (wood)",
-        description:
-            "Базовый вектор развития не даёт нам иного выбора, кроме определения новых предложений.",
-    },
-];
+    ],
+    error: null,
+}
 
 const artefactSlice = createSlice({
-    name: 'artefactSlice',
+    name: 'artefact',
     initialState,
     reducers: {
-        addArtefact: (state, action) => {
-            state.push(action.payload)
+        addArtefact: (state, action: PayloadAction<Artefact>) => {
+            const exist = state.artefacts.some(artefact => artefact.title === action.payload.title);
+            if(exist) {
+                return {...state, error: 'Артефакт уже существует'};
+            }
+            return {...state, artefacts: [...state.artefacts, action.payload], error: null};
         }
     }
 });
---
+++ src/types/artefacts-state.ts
@@ -0,0 +1,6 @@
+import {Artefact} from "./artefact.ts";
+
+export interface ArtefactsState {
+    artefacts: Artefact[];
+    error: null | string;
+}
